36e29898699f3bf1dde29eb5e2a6b6465002399b
sxuhms <heath_shults@fanniemae.com>
2018-10-11 15:50:10 -0500

    install

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const open = require('open')
+const app = express()
+const port = 4000
+const host = localhost
+
+app.get('/', (req, res) => res.send(''))
+
+app.listen(port, host, () => {
+  console.log(`Example app listening on port ${port}!`))
+  open('http://localhost:4000')
+}
