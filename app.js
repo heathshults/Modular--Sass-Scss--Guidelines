8084ff9f731090993650f37bc96a1e0fe5d6663f
sxuhms <heath_shults@fanniemae.com>
2018-10-11 16:02:26 -0500

    installed express

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express')
 const open = require('open')
 const app = express()
 const port = 4000
-const host = localhost
+const host = 'localhost'
 
 app.get('/', (req, res) => res.send(''))
 
 app.listen(port, host, () => {
-  console.log(`Example app listening on port ${port}!`))
-  open('http://localhost:4000')
-}
+  console.log(`Example app listening on port ${port}!`)
+  open('localhost:4000')
+})
